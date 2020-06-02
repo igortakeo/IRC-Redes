4999561d88a3a8757d8824600e4b7b4d9d4118bd
Igor Takeo <48393805+igortakeo@users.noreply.github.com>
2020-06-01 22:25:36 -0300

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -23,7 +23,7 @@ int main(){
 	}
 
 	ServerAddress.sin_family = AF_INET;
-	ServerAddress.sin_port = htons(5000);
+	ServerAddress.sin_port = htons(1048);
 	ServerAddress.sin_addr.s_addr = inet_addr("159.89.214.31");	
 	//Conectando o cliente a porta 8080
 	int retConnect = connect(NewSocket, (struct sockaddr*)&ServerAddress, sizeof ServerAddress);
